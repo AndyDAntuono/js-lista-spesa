2541d039f1483f921ef427df6c410dde5338e74f
AndyDAntuono <andrea.dantuono88@gmail.com>
2024-07-02 17:57:32 +0200

    [UPD] updated both index.html and script.js files

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 
-
+/* [RIFERIMENTO]
 // HO UNA LISTA DELLA SPESA, DEVO MOSTRALA A VIDEO
 const ingredienti = ["Guanciale", "Peperoni", "Pasta", "Olive", "Controfiletto", "Quaglie", "Pollo", "Pomodori", "Melanzane"];
 
 // RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA MIA LISTA DELLA SPESA
 const lista_spesa = document.getElementById("lista_spesa")
+console.log(lista_spesa)
 
 // CICLO L'ARRAY
 for (let i = 0; i < ingredienti.length; i++) {
@@ -12,58 +13,19 @@ for (let i = 0; i < ingredienti.length; i++) {
     const li = document.createElement("li");
 
     //GLI METTO IL TESTO ALL'INTERNO
-    li.innerTEXT = ingredienti[i];
+    li.innerText = ingredienti[i];
 
     // APPENDO IL TEST ITEM ALL'INTERNO DELLA LISTA DELLA SPESA
     lista_spesa.append(li)
 }
-
-
-
-
-/*[TENTATIVO n°2]*/
-/*
-// HO UNA LISTA DELLA SPESA, DEVO MOSTRALA A VIDEO
-const ingredienti = ["Guanciale", "Peperoni", "Pasta", "Olive", "Controfiletto", "Quaglie", "Pollo", "Pomodori", "Melanzane"];
-
-// RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA MIA LISTA DELLA SPESA
-const lista_spesa = document.getElementById("lista_spesa")
-
-function make_list (list) {
-    let i = 0;
-
-    while (i < ingredienti.length) {
-    i++
-
-    // CREO UN NUOVO LIST ITEM
-    const li = document.createElement("li");
-
-    // GLI METTO IL TESTO ALL'INTERNO
-    li.innerText = ingredienti[i];
-
-    // APPENDO IL LIST ITEM ALL'INTERNO DELLA LISTA DELLA SPESA
-    lista_spesa.append(li);
-
-    }
-    
-    return ingredienti
-
-    
-}
-
-let result = make_list(ingredienti);
-console.log(result);
 */
 
-
-
-/*
-[TENTATIVO n°1]
 // HO UNA LISTA DELLA SPESA, DEVO MOSTRALA A VIDEO
 const ingredienti = ["Guanciale", "Peperoni", "Pasta", "Olive", "Controfiletto", "Quaglie", "Pollo", "Pomodori", "Melanzane"];
 
 // RECUPERO L'ELEMENTO CHE DEVE CONTENERE LA MIA LISTA DELLA SPESA
 const lista_spesa = document.getElementById("lista_spesa")
+console.log(lista_spesa)
 
 // CICLO L'ARRAY
 let i = 0;
@@ -82,4 +44,3 @@ while (i < ingredienti.length) {
     lista_spesa.append(li);
 
 }
-*/
